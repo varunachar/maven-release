489a6e62b2f2a9ba1333a7419a8ff3989595e858
Varun Achar <achar_varun@hotmail.com>
2013-03-01 10:47:03 +0530

    Importing maven-release project

---
+++ commons/src/main/java/com/trelta/commons/App.java
@@ -0,0 +1,13 @@
+package com.trelta.commons;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
